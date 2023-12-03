bee0855418d07f9a8475861680849dd130c8bee3
gotied <gautierdelbet80@gmail.com>
2023-12-03 23:32:18 +0100

    initial commit

---
+++ symfony/filmo/src/Entity/Personne.php
@@ -0,0 +1,118 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PersonneRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PersonneRepository::class)]
+class Personne
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $prenom = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $nom = null;
+
+    #[ORM\ManyToMany(targetEntity: Produit::class, mappedBy: 'personne')]
+    private Collection $produits;
+
+    #[ORM\ManyToMany(targetEntity: Role::class, mappedBy: 'personne')]
+    private Collection $roles;
+
+    public function __construct()
+    {
+        $this->produits = new ArrayCollection();
+        $this->roles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): static
+    {
+        $this->prenom = $prenom;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Produit>
+     */
+    public function getProduits(): Collection
+    {
+        return $this->produits;
+    }
+
+    public function addProduit(Produit $produit): static
+    {
+        if (!$this->produits->contains($produit)) {
+            $this->produits->add($produit);
+            $produit->addPersonne($this);
+        }
+
+        return $this;
+    }
+
+    public function removeProduit(Produit $produit): static
+    {
+        if ($this->produits->removeElement($produit)) {
+            $produit->removePersonne($this);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Role>
+     */
+    public function getRoles(): Collection
+    {
+        return $this->roles;
+    }
+
+    public function addRole(Role $role): static
+    {
+        if (!$this->roles->contains($role)) {
+            $this->roles->add($role);
+            $role->addPersonne($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRole(Role $role): static
+    {
+        if ($this->roles->removeElement($role)) {
+            $role->removePersonne($this);
+        }
+
+        return $this;
+    }
+}
---
+++ symfony/filmo/src/Entity/Produit.php
@@ -0,0 +1,221 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProduitRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ProduitRepository::class)]
+class Produit
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100, nullable: true)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $synopsis = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $date_sortie = null;
+
+    #[ORM\Column(length: 10, nullable: true)]
+    private ?string $duree = null;
+
+    #[ORM\Column(length: 50, nullable: true)]
+    private ?string $image = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2, nullable: true)]
+    private ?string $prix_ht = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 6, scale: 2, nullable: true)]
+    private ?string $prix_four = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $stock = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $actif = null;
+
+    #[ORM\ManyToMany(targetEntity: Personne::class, inversedBy: 'produits')]
+    private Collection $personne;
+
+    #[ORM\ManyToMany(targetEntity: Role::class, mappedBy: 'produit')]
+    private Collection $roles;
+
+    public function __construct()
+    {
+        $this->personne = new ArrayCollection();
+        $this->roles = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(?string $titre): static
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getSynopsis(): ?string
+    {
+        return $this->synopsis;
+    }
+
+    public function setSynopsis(?string $synopsis): static
+    {
+        $this->synopsis = $synopsis;
+
+        return $this;
+    }
+
+    public function getDateSortie(): ?\DateTimeInterface
+    {
+        return $this->date_sortie;
+    }
+
+    public function setDateSortie(?\DateTimeInterface $date_sortie): static
+    {
+        $this->date_sortie = $date_sortie;
+
+        return $this;
+    }
+
+    public function getDuree(): ?string
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(?string $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(?string $image): static
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getPrixHt(): ?string
+    {
+        return $this->prix_ht;
+    }
+
+    public function setPrixHt(?string $prix_ht): static
+    {
+        $this->prix_ht = $prix_ht;
+
+        return $this;
+    }
+
+    public function getPrixFour(): ?string
+    {
+        return $this->prix_four;
+    }
+
+    public function setPrixFour(?string $prix_four): static
+    {
+        $this->prix_four = $prix_four;
+
+        return $this;
+    }
+
+    public function getStock(): ?int
+    {
+        return $this->stock;
+    }
+
+    public function setStock(?int $stock): static
+    {
+        $this->stock = $stock;
+
+        return $this;
+    }
+
+    public function isActif(): ?bool
+    {
+        return $this->actif;
+    }
+
+    public function setActif(?bool $actif): static
+    {
+        $this->actif = $actif;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Personne>
+     */
+    public function getPersonne(): Collection
+    {
+        return $this->personne;
+    }
+
+    public function addPersonne(Personne $personne): static
+    {
+        if (!$this->personne->contains($personne)) {
+            $this->personne->add($personne);
+        }
+
+        return $this;
+    }
+
+    public function removePersonne(Personne $personne): static
+    {
+        $this->personne->removeElement($personne);
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Role>
+     */
+    public function getRoles(): Collection
+    {
+        return $this->roles;
+    }
+
+    public function addRole(Role $role): static
+    {
+        if (!$this->roles->contains($role)) {
+            $this->roles->add($role);
+            $role->addProduit($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRole(Role $role): static
+    {
+        if ($this->roles->removeElement($role)) {
+            $role->removeProduit($this);
+        }
+
+        return $this;
+    }
+}
